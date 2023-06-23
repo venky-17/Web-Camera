56e87f5f84814c74da32f81b88b0582782dd5caf
Venkatesh K <128626606+venky-17@users.noreply.github.com>
2023-06-23 10:07:21 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,106 @@
+ const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video:true, audio:false})
+.then(localMediaStream => {
+    video.srcObject = localMediaStream
+    video.play();
+ })
+ .catch(err =>{
+    alert("there was a problem in accessing your camera", err)
+ })
+}
+function dumpToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    console.log(height,width);
+    canvas.width = width;
+    canvas.height = height;
+
+
+   return setInterval(() => {
+        ctx.drawImage(video, 0,0,width,height)
+      let pixels = ctx.getImageData(0,0,width,height)
+        
+      //pixels = redEffect(pixels)
+      pixels = rgbSplit(pixels)
+      //ctx.globalAlpha = 0.3  ;
+      ctx.putImageData(pixels,0,0)
+    
+
+    },16)
+
+}
+function clickPhoto(){
+    //to play sound
+    snap.currentTime=0;
+    snap.play();
+
+
+const data = canvas.toDataURL('image/jpeg');
+const link = document.createElement('a');
+link.href = data;
+link.setAttribute('download','camera')
+link.innerHTML= `<img src="${data}" alt="ur image"/>`;
+strip.insertBefore(link, strip.firstChild)
+
+}
+
+function redEffect(pixels){
+    for(i=0; i < pixels.data.length; i+= 4){
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+        
+    }
+ 
+     return pixels;
+}
+
+function rgbSplit(pixels){
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+      }
+      return pixels;
+}
+
+
+function greenScreen(pixels){
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+}
+
+getVideo()
+
+
+video.addEventListener('canplay', dumpToCanvas)
